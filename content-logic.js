bb21cda9e12d1791c4fa2ed6be05f80f9c647886
seojihyuk26 <seojihyuk26@gmail.com>
2022-01-21 14:41:29 +0900

    add brdUrl function

--- content-logic.js
+++ content-logic.js
@@ -20,7 +20,30 @@ function injectCode(src) {
 
 injectCode(chrome.runtime.getURL('/closeConfirm.js'));
 
+function getNewbrdUrl(url,brdNum){
+    fetch('https://www.mofa.go.kr/www/main.do')
+    .then(function(response) {
+        // When the page is loaded convert it to text
+        return response.text()
+    })
+    .then(function(html) {
+        // Initialize the DOM parser
+        var parser = new DOMParser();
 
+        // Parse the text
+        var doc = parser.parseFromString(html, "text/html");
+
+        // You can now even select part of that html as you would in the regular DOM 
+        // Example:
+        var brdListRelativeUrl = doc.querySelectorAll('li.wow.fadeIn')[1].querySelector('a').pathname;
+        brdUrl = url+brdListRelativeUrl+"?seq="+brdNum;
+        console.log(brdUrl);
+    })
+    .catch(function(err) {  
+        console.log('Failed to fetch page: ', err);  
+    });
+    return brdUrl;
+}
 
 function addZero(v) {
     return (v < 10 ? v = '0' + v : v);
